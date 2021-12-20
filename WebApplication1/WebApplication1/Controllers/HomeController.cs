af4e98cb958f7e4b8be824eabe4e09faedb5c963
Ahadvt <ahadvt@code.edu.az>
2021-12-20 05:56:26 +0400

    done

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Acordion()
+        { 
+            return View();
+        }
+
+        public ActionResult Progresbar()
+        {
+            return View();
+        }
+    }
+}
